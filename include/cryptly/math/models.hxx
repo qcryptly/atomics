afd95120cbe0a106faedcf4de694343c4eac54a2
Taylor Kaplan <qcryptly@gmail.com>
2019-09-01 22:27:36 -0600

    :art: Refactor ops and fxns to live in models
    [Description]
    We've figured out the interface for calculating physical phenomina
    and performing independent model energy calculations. For this commit
    we are putting derivatives and more complex ops in model ops.
    
    We start with a notion of model which can be a basis function used
    for the thing we want to predict. Models will live in normal fxn
    namespace for lack of a better idea.

---
+++ include/cryptly/math/models.hxx
@@ -0,0 +1,272 @@
+#pragma once
+
+#include <complex>
+#include <algorithm>
+#include <type_traits>
+#include <functional>
+
+namespace cryptly::math {
+inline namespace v1 {
+
+using precision_t = long double;
+
+// Throw the scientific constants up here
+namespace physical_units {
+    constexpr long double pi = 3.141592653589793238L;
+    constexpr long double reduced_bohr_radius = 5.2917721067e-11;
+    constexpr long double planks_constant_joules = 4.135667696e-15;
+    constexpr long double reduced_planks_constant_joules = 1.054571817e-34; 
+    constexpr long double electron_mass = 9.10938356e-31; // In kg 
+    constexpr long double proton_mass = 1.6726219e-27; // In kg
+    constexpr long double electron_charge = 1.602176634e-19; // In columbs
+    constexpr long double free_perm = 8.8541878128e-12; // C^2 / N m2 --> N = kg m / s^2
+    constexpr long double hydrogen_reduced_mass = (electron_mass * proton_mass) / (electron_mass + proton_mass);
+    constexpr long double hamiltonian_potential_const = (electron_charge * electron_charge) / (4 * pi * free_perm);
+    constexpr long double hamiltonian_kinetic_const = (reduced_planks_constant_joules * reduced_planks_constant_joules) / (2.0 * hydrogen_reduced_mass); 
+}
+////////////////////////////////////////////////////
+// Define some common functions
+template<class T>
+constexpr T factorial(T n) {
+    return n <= 1 ? 1 : (n * factorial(n - 1));
+}
+
+// Force compiler to instantiate these lookups
+template<class T>
+void __hack_impl_fact(){
+  T fact{};
+  std::size_t i{};
+  do {
+    fact = factorial(i);
+  } while((fact / std::numeric_limits<T>::max()) * ++i < 1);
+}
+
+// Tricks compiler into implementing the factorial at compile
+// time.
+void __hack_impl(){
+  // Create constant expression lookup table
+  __hack_impl_fact<std::uint64_t>();
+  __hack_impl_fact<std::uint32_t>();
+  __hack_impl_fact<std::int64_t>();
+  __hack_impl_fact<std::int32_t>();
+}
+///////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////
+// This will work as our numerics lib...
+// which includes useful directions and ops
+namespace nm {
+  using index_t = int;
+  template <class T>
+    struct is_complex {
+	  typedef char one;
+	  typedef struct { char x[2]; } two;
+	  template <class C>
+	  static one test (int C::*imag);
+	  template <class C>
+	  static two test (...);
+	  public:
+		enum {value = sizeof(is_complex<T>::test<T>(0)) == 1};
+  };
+
+  // Easy directions
+  namespace cart_coor {
+    namespace orth_dir {
+      constexpr auto x_1 = std::make_tuple<index_t>(1);
+
+      constexpr auto x_2 = std::make_tuple<index_t,index_t>(1,0);
+      constexpr auto y_2 = std::make_tuple<index_t,index_t>(0,1);
+
+      constexpr auto x_3 = std::make_tuple<index_t,index_t,index_t>(1,0,0);
+      constexpr auto y_3 = std::make_tuple<index_t,index_t,index_t>(0,1,0);
+      constexpr auto z_3 = std::make_tuple<index_t,index_t,index_t>(0,0,1);
+    }
+  }
+
+  namespace sph_coor {
+    namespace orth_dir {
+      constexpr auto ro_i_3 = 0;
+      constexpr auto theta_i_3 = 1;
+      constexpr auto phi_i_3 = 2;
+      constexpr auto ro_3 = std::make_tuple<index_t,index_t,index_t>(1,0,0);
+      constexpr auto theta_3 = std::make_tuple<index_t,index_t,index_t>(0,1,0);
+      constexpr auto phi_3 = std::make_tuple<index_t,index_t,index_t>(0,0,1);
+    }
+  }
+
+  template<class FType, class PType, class DVType, class HType, auto... Ints>
+  constexpr auto derivative_impl(
+    const FType fxn, 
+    const PType point, 
+    const DVType derivative_vect, 
+    const HType h, const std::index_sequence<Ints...>) {
+      PType _xhr = std::make_tuple((std::get<Ints>(point) + (static_cast<HType>(std::get<Ints>(derivative_vect))*h))...);
+      PType _xhl = std::make_tuple((std::get<Ints>(point) - (static_cast<HType>(std::get<Ints>(derivative_vect))*h))...);
+      const auto diff = fxn(_xhr) - fxn(_xhl);
+      if constexpr(is_complex<decltype(diff)>::value) {
+        return diff / std::complex<decltype(diff.imag())>{2*h};
+      } else {
+        return diff / (2*h);
+      }
+  };
+
+  template<class FType, class PType, class DVType, class HType, auto... Ints>
+  constexpr auto derivative_forward_impl(
+    const FType fxn, 
+    const PType point, 
+    const DVType derivative_vect, 
+    const HType h, const std::index_sequence<Ints...>) {
+      PType _xhr = std::make_tuple((std::get<Ints>(point) + (static_cast<HType>(std::get<Ints>(derivative_vect))*h))...);
+      const auto diff = fxn(_xhr) - fxn(point);
+      if constexpr(is_complex<decltype(diff)>::value) {
+        return diff / std::complex<decltype(diff.imag())>{h};
+      } else {
+        return diff / h;
+      }
+  };
+
+  template<bool Central = true>
+  constexpr auto derivative = [](
+    const auto fxn, 
+    const auto point, 
+    const auto derivative_vect, 
+    const auto h){
+      if constexpr(Central) {
+          return derivative_impl(
+            fxn, point, derivative_vect, 
+            h, std::make_index_sequence<std::tuple_size_v<decltype(point)>>{});
+      } else {
+          return derivative_forward_impl(
+            fxn, point, derivative_vect, 
+            h, std::make_index_sequence<std::tuple_size_v<decltype(point)>>{});
+      }
+  };
+
+  // We need a concept for complex conjugate vs real numbers
+  template<bool Central = true>
+  auto derivative_err = [](
+    const auto fxn, 
+    const auto point, 
+    const auto derivative_vect,
+    const auto err) {
+      long double h{};
+      long double h2{1.0};
+      auto fd1 = nm::derivative<Central>(fxn, point, derivative_vect, h);
+      auto fd2= nm::derivative<Central>(fxn, point, derivative_vect, h);
+      auto fd_err = std::abs(fd1 - fd2) / std::abs(fd2);
+      do {
+        h = h2;
+        h2 /= 10.0;
+        fd1 = nm::derivative<Central>(fxn, point, derivative_vect, h);
+        fd2 = nm::derivative<Central>(fxn, point, derivative_vect, h2);
+        fd_err = std::abs(fd1 - fd2) / std::abs(fd2);
+      } while(fd_err > err);
+      return fd2;
+  };
+
+  template<bool Central = true>
+  auto lazy_derivative_err = [](
+    const auto fxn, 
+    const auto point, 
+    const auto d_vect, 
+    const auto err) {
+      return [=](const auto _point) {
+        return derivative_err<Central>(fxn, _point, d_vect, err);
+      };
+  };
+
+  template<bool Central = true, auto N, class TFxn, class TPoint, class ...TArgs>
+  auto n_derivative_err(
+    const TFxn fxn,
+    const TPoint point,
+    const TArgs... args) {
+      if constexpr(N < 1) {
+        return fxn(point);
+      } else {
+        const auto ld = nm::lazy_derivative_err<Central>(fxn, point, args...);
+        return n_derivative_err<Central, N - 1, decltype(ld), TPoint, TArgs...>(
+          ld,
+          point,
+          args...
+        );
+      }
+  };
+
+  namespace ops {
+    auto spherical_lap = [](
+      const auto fxn, 
+      const auto point, 
+      const auto err){
+        const auto[ro, theta, phi] = point;
+        constexpr auto ro_d = sph_coor::orth_dir::ro_3;
+        constexpr auto theta_d = sph_coor::orth_dir::theta_3;
+        constexpr auto phi_d = sph_coor::orth_dir::phi_3;
+        const auto fxn_rf = [_fxn = fxn](const auto _point) {
+          return std::get<sph_coor::orth_dir::ro_i_3>(_point) * _fxn(_point);
+        };
+        const auto fxn_sdf = [_fxn = fxn, _theta_d = theta_d, _err = err](const auto _point) {
+          return std::sin(std::get<sph_coor::orth_dir::theta_i_3>(_point)) * derivative<false>(
+            _fxn, _point, _theta_d, _err);
+        };
+        return (1.0/ro)*n_derivative_err<false, 2,
+            decltype(fxn_rf), decltype(point),
+            decltype(ro_d), decltype(err)>(
+          fxn_rf, point, ro_d, err) +
+          (1.0/(std::pow(ro,2) * std::sin(theta))) *
+          derivative_err<true>(fxn_sdf, point, theta_d, err) +
+          (1.0/(std::pow(ro,2) * std::pow(std::sin(theta), 2))) *
+          n_derivative_err<false, 2,
+            decltype(fxn), decltype(point),
+            decltype(phi_d), decltype(err)>(
+          fxn, point, phi_d, err);
+    };
+
+    auto hamiltonian = [](
+      const auto wavefunction, 
+      const auto point, 
+      const auto err){
+        const auto[ro, theta, phi] = point;
+        constexpr auto ro_d = sph_coor::orth_dir::ro_3;
+        constexpr auto theta_d = sph_coor::orth_dir::theta_3;
+        constexpr auto phi_d = sph_coor::orth_dir::phi_3;
+        return (-1)*physical_units::hamiltonian_kinetic_const * spherical_lap(wavefunction, point, err) - physical_units::hamiltonian_potential_const * (1.0/ro) * wavefunction(point);
+    };
+  }
+}
+// End of ops namespace
+namespace models {
+	using scalar_t = long double;
+	using point_t = std::tuple<scalar_t, scalar_t, scalar_t>;
+	auto lag = [](std::uint64_t l, std::uint64_t n, long double ro){
+	  return std::assoc_laguerre((2*l + 1), n - l - 1, ro) ;
+	};
+
+	auto sph_fact = [](std::uint64_t l, std::int64_t m){
+	  return std::sqrt(precision_t((2*l + 1)*factorial(l - m)) /
+			  4.0 * physical_units::pi * precision_t(factorial(l + m)));
+	};
+
+	auto sph = [](std::uint64_t l, std::int64_t m, long double theta, long double phi){
+	  return sph_fact(l, m)*std::assoc_legendre(l, m, std::cos(theta))*std::exp(std::complex<long double>{m * phi, 0});
+	};
+
+	auto wf_fact = [](std::uint64_t n, std::uint64_t l, long double ro){
+	  return std::sqrt(
+		std::pow((2.0/(static_cast<precision_t>(n)*physical_units::reduced_bohr_radius)), 3) *
+		precision_t(factorial(n - l - 1))/(2*n*factorial(n + l))
+	  ) * std::exp((-1 * ro) / 2) * std::pow(ro, l);
+	};
+
+	auto wf_sem_simple = [](const auto hyper_param) {
+		const auto[_n,_l,_m] = hyper_param;
+		return [n = _n, l = _l, m = _m](point_t point){
+			const auto[ro, theta, phi] = point;	
+			return wf_fact(n,l,ro) * lag(l, n, ro) * sph(l, m, theta, phi);
+		};
+	};
+}
+
+// End of models namespace
+
+}
+}
---
+++ include/cryptly/math/models.spec.cxx
@@ -0,0 +1,82 @@
+#include <tuple>
+#include <iostream>
+
+#include "cryptly/math/models.hxx"
+
+#include "cryptly/test.hxx"
+
+TEST(Test, derivative) {
+	using namespace cryptly::math;
+	using point_t = std::tuple<long double>;
+	constexpr auto d_vect = nm::cart_coor::orth_dir::x_1;
+	constexpr auto err_near = 1.0e-4;
+	constexpr auto err{1.0e-6};
+	point_t point{3};
+
+	auto my_fxn = [](const auto point){
+		const auto x_value = std::get<0>(point);
+		return (std::pow(x_value, 2)*std::sin(x_value))/std::log(x_value);
+	};
+	const auto result = nm::derivative<true>(my_fxn, point, d_vect, err); 
+	const auto second_result = nm::derivative_err<true>(
+		nm::lazy_derivative_err<true>(my_fxn, point, d_vect, err),
+		point, d_vect, err);
+
+	const auto n_first_result = nm::n_derivative_err<true, 1,
+		decltype(my_fxn), decltype(point),
+		decltype(d_vect), decltype(err)>(
+			my_fxn, point, d_vect, err);
+	const auto n_second_result = nm::n_derivative_err<true, 2,
+		decltype(my_fxn), decltype(point),
+		decltype(d_vect), decltype(err)>(
+			my_fxn, point, d_vect, err);
+	const auto n_third_result = nm::n_derivative_err<true, 3,
+		decltype(my_fxn), decltype(point),
+		decltype(d_vect), decltype(err)>(
+			my_fxn, point, d_vect, err);
+
+	EXPECT_NEAR(result, -7.69022, err_near);
+	EXPECT_NEAR(second_result, -6.92918, err_near);
+
+	EXPECT_NEAR(n_first_result, -7.69022, err_near);
+	EXPECT_NEAR(n_second_result, -6.92918, err_near);
+	EXPECT_NEAR(n_third_result, 4.287186, err_near);
+}
+
+TEST(Test, SingleElectronModel) {
+	using namespace cryptly::math;
+	constexpr auto err{1.0e-6};
+	// n, l, m
+	const auto quantum_numbers = std::make_tuple(2,1,0); 
+	const auto coordinates = std::make_tuple<long double, long double, long double>(1.0, 0.4, 0.3);
+    const auto [ro, theta, phi] = coordinates;
+	auto wf = models::wf_sem_simple(quantum_numbers);
+	const auto result = wf(coordinates);
+	std::cout << "Wave function: " << result << std::endl;
+	const auto op_results = nm::ops::spherical_lap(wf, coordinates, err);
+	std::cout << "Spherical Lap applied: " << op_results << std::endl;
+    const auto ham_results = nm::ops::hamiltonian(wf, coordinates, err);
+    std::cout << "Hamiltonian applied: " << ham_results << std::endl;
+    const auto expect = std::conj(wf(coordinates)) * ham_results;
+    std::cout << "Expectation: " << expect << std::endl;
+    const auto expect_space = expect * std::pow(ro, 2) * std::sin(phi);
+    std::cout << "Expected space: " << expect_space << std::endl;
+  // n, l, m, ro, theta, phi
+//  using fxns = cryptly::math::models::fxns;
+//  using ops = cryptly::math::models::ops;
+//  const auto electron_numbers = std::make_tuple(2,1,0);
+//  const auto electron_position = std::make_tuple(0.0,0.0,0.0);
+//  // Lazy lambda
+//  const auto sem = fxns::sem(qm_numbers);
+//  // Conjugate sem
+//  const auto sem_i = ops::conj(fxns::sem, electron_numbers);
+//  const auto ops_sph_lap = ops::sph_lap;
+//  long double err = 1.0e-6;
+//  std::complex<long double> expected_value{0,0};
+//  const auto res_1 = sph_lap(sem, electron_position, err);
+//  // Due to eager computation, we can just use res_1;
+//  const auto res_2 = sem_i(electron_position) * res_1;
+//  EXPECT_EQ(sem(electron_position), expected_value);
+//  EXPECT_EQ(res_1, expected_value); 
+//  EXPECT_EQ(res_2, expected_value); 
+}
